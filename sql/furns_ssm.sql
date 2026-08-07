204ff44a6627daebc4602171178b964d6639902c
MrHuai <andbreak@outlook.com>
2026-08-07 16:55:30 +0800

    list接口与sql语句

---
+++ sql/furns_ssm.sql
@@ -0,0 +1,43 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本地链接
+ Source Server Type    : MySQL
+ Source Server Version : 50733
+ Source Host           : localhost:3306
+ Source Schema         : furns_ssm
+
+ Target Server Type    : MySQL
+ Target Server Version : 50733
+ File Encoding         : 65001
+
+ Date: 07/08/2026 16:00:39
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for furn
+-- ----------------------------
+DROP TABLE IF EXISTS `furn`;
+CREATE TABLE `furn`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `marker` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `price` decimal(11, 2) NOT NULL,
+  `sales` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `img_path` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of furn
+-- ----------------------------
+INSERT INTO `furn` VALUES (1, '简约风格小椅子', '熊猫家具', 180.00, 666, 7, NULL);
+INSERT INTO `furn` VALUES (2, '北欧风格小椅子', '蚂蚁家具', 180.00, 666, 7, NULL);
+INSERT INTO `furn` VALUES (3, '小板凳', '顾家家居', 100.00, 10, 200, '');
+INSERT INTO `furn` VALUES (5, '丫丫', '丫丫家具', 120.00, 20, 90, '');
+
+SET FOREIGN_KEY_CHECKS = 1;
